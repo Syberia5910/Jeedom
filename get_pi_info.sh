68378921c0be0591ceef9668d85bf3edf0ed9b5b
Syberia5910 <nicolas.lecaille@wanadoo.fr>
2021-06-25 20:42:26 +0200

    Create get_pi_info.sh

---
+++ get_pi_info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Usage: sh-pi-get_cpu_temp
+#
+# Monitors CPU clock speed and temperature.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see &lt;http://www.gnu.org/licenses/&gt;.
+#
+cpu_freq=/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
+cpu_temp=/sys/class/thermal/thermal_zone0/temp
+if [ -e $cpu_freq ] ; then
+  if [ -e $cpu_temp ] ; then
+    while [ true ] ; do
+        clk=$(cat $cpu_freq)
+        cpu=$(cat $cpu_temp)
+        echo -ne $(($clk/1000))" Mhz / "$(($cpu/1000))" C \r" 
+        echo "gpu &gt; " $(/opt/vc/bin/vcgencmd measure_temp)
+        sleep 1
+        clear
+    done
+   fi
+fi
